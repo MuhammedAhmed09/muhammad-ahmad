fd3f31b7ab4f766b1267ad58cf93a231b464a61e
[Muhammed] <[nameismilo2@gmail.com]>
2025-10-26 21:25:45 +0300

    finish works-section

---
+++ app/components/projectslides.jsx
@@ -0,0 +1,48 @@
+'use client'
+import { useEffect, useState } from 'react';
+
+//swiper
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination, Navigation } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+
+const Projects = ()  => {
+    const [projectItems, setProjectItems] = useState([]);
+
+    useEffect(() => {
+        fetch('/files/projects.json')
+        .then((res) => res.json())
+        .then((data) => setProjectItems(data))
+    }, []);
+
+  return (
+    <Swiper
+        slidesPerView={1}
+        loop={true}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        modules={[Pagination, Navigation]}
+    >
+        {projectItems.map((project) => (
+            <SwiperSlide key={project.id} className="w-full px-[2%] sm:px-[4%] md:p-0 ">
+                <div className="flex items-end flex-row justify-center gap-4 px-[12%] sm:px-[14%]">
+                    <p className="bg-Charcoal text-white w-2/5 text-sm rounded-2xl p-4">
+                        {project.description}
+                    </p>
+                    <img
+                        src={project.image}
+                        alt={project.title}
+                        className="w-3/5 rounded-2xl p-2 object-cover"
+                    />
+                </div>
+            </SwiperSlide>
+        ))}
+    </Swiper>
+  )
+}
+
+export default Projects
--- app/works/page.js
+++ app/works/page.js
@@ -1,7 +1,22 @@
+import Projects from "../components/projectslides";
+
 export default function Works() {
+  
     return (
-    <section id="works" className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <h3 className="text-Aqua-Neon font-bold text-3xl">work</h3>
+     <section id="works" className={`flex py-8 gap-8 flex-col min-h-screen text-white items-center justify-center font-sans bg-[url('/images/works-bg.jpg')] bg-cover bg-no-repeat`}>
+        <div className="hidden md:flex">
+          <img src="/images/Scroll.png" alt="scroll" className="w-1/2" />
+        </div>
+      <div className="px-[14%] lg:px-[8%]">
+        <div className="items-center gap-4 text-center flex flex-col justify-center">
+          <p className="text-Aqua-Neon text-3xl md:text-4xl">Works</p>
+          <img src="images/line.png" alt="line" className="text-Aqua-Neon w-20 md:w-30" />
+          <p className="text-white/70 text-sm">I had the pleasure of working with these awesome projects</p>
+        </div>
+      </div>
+      <div className="w-full md:w-lg">
+        <Projects />
+      </div>
     </section>
     )
 }
